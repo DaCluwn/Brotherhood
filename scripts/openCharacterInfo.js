95bef47de9c40ce5a36987c5d26ba8c17ebb5fd7
DaCluwn <39062069+DaCluwn@users.noreply.github.com>
2023-10-20 16:31:31 +0300

    Testing out character display

---
+++ scripts/openCharacterInfo.js
@@ -0,0 +1,16 @@
+window.onload = async function displayCharacterInfo(){
+    const urlParams = new URLSearchParams(window.location.search);  
+    const dataset = urlParams.get('dataset');
+    console.log(dataset);
+    const index = urlParams.get('index');
+    console.log(index);
+    let data={}
+    try {
+        await fetch(dataset).then(response => {if (!response.ok) { throw new Error("HTTP error " + response.status);}return response.json();}).then(json => {data = json[index];})
+    }catch (e) {
+        console.error(e,e.stack);
+    }
+    document.getElementById("characterName").innerHTML=data.name
+    document.getElementById("characterRace").innerHTML="Race: "+data.race
+    
+}
