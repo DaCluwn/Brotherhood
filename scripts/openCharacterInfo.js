d72ef7351723619d32aa6a24a9f7c4ef9e5163b9
DaCluwn <39062069+DaCluwn@users.noreply.github.com>
2023-10-20 16:54:20 +0300

    update

--- scripts/openCharacterInfo.js
+++ scripts/openCharacterInfo.js
@@ -1,17 +1,25 @@
-let data={}
+var data
 
 window.onload = async function displayCharacterInfo(){
     const urlParams = new URLSearchParams(window.location.search);  
+
     const dataset = urlParams.get('dataset');
     console.log(dataset);
+
     const index = urlParams.get('index');
     console.log(index);
+
+    await FetchData(dataset,index)
+
+    document.getElementById("characterName").innerHTML=data.name
+    document.getElementById("characterRace").innerHTML="Race: "+data.race
+    
+}
+
+async function FetchData(dataset,index){
     try {
         await fetch("../"+dataset).then(response => {if (!response.ok) { throw new Error("HTTP error " + response.status);}return response.json();}).then(json => {data = json[index];})
     }catch (e) {
         console.error(e,e.stack);
     }
-    document.getElementById("characterName").innerHTML=data.name
-    document.getElementById("characterRace").innerHTML="Race: "+data.race
-    
 }
