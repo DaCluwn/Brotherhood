2d838c6c200edbda304ccd3892c3ebb0e98d85ab
DaCluwn <39062069+DaCluwn@users.noreply.github.com>
2023-10-20 16:47:17 +0300

    Update openCharacterInfo.js

--- scripts/openCharacterInfo.js
+++ scripts/openCharacterInfo.js
@@ -4,8 +4,8 @@ window.onload = async function displayCharacterInfo(){
     console.log(dataset);
     const index = urlParams.get('index');
     console.log(index);
-    let data={}
-    try {
+    await let data={}
+    await try {
         await fetch("../"+dataset).then(response => {if (!response.ok) { throw new Error("HTTP error " + response.status);}return response.json();}).then(json => {data = json[index];})
     }catch (e) {
         console.error(e,e.stack);
